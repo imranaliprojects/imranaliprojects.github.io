844f0867a05aa8fb503c2f7cf70e1e3bc1f517ec
Imran <08.imran.ali.03@gmail.com>
2025-01-16 13:30:21 -0600

    Added more code to code snippet.

--- R/Code Snippets/Basic Analysis of mtcars Dataset.R
+++ R/Code Snippets/Basic Analysis of mtcars Dataset.R
@@ -27,15 +27,30 @@ barplot(df$`Average MPG`, names.arg = df$`# of Cylinders`,
 #Which car had the largest HP/Weight Ratio?
 #------------------------------------------
 
+#deframe from: 
+#https://stackoverflow.com/questions/73934726/turn-a-data-frame-column-into-vector-with-names-as-row-names
+
 df2 <- mtcars[c(4,6)] #Remove Columns not needed for power-to-weight ratio
 pwr.to.weight <- df2[1]/df2[2] #Calculate power-to-weight ratio
-
-pwr.to.weight #Check Column name
-?mtcars #To check the units, discovered weight is in 1000lbs units
 pwr.to.weight <- pwr.to.weight/1000 #Corrected values to HP/LBS
 names(pwr.to.weight) <- c("pwr-to-wt")
 
 #Find vehicle with largest power-to-weight ratio
 row.names(pwr.to.weight)[which.max(pwr.to.weight$'pwr-to-wt')]
 
+
+
+#----------------------------------------------------------------
+#Plot the top five vehicles with the best power-to-weight ratios.
+#----------------------------------------------------------------
+
+#Using (https://www.programmingr.com/examples/r-dataframe/sort-r-data-frame/),
+#Find the top five vehicles with largest pwr/wt ratio:
+top.five<-pwr.to.weight[order(-pwr.to.weight$'pwr-to-wt'),][1:5]#From website
+
+#Adding in vehicle model names:
+names(top.five)<-row.names(pwr.to.weight)[order(-pwr.to.weight$'pwr-to-wt')[1:5]]
+
+#Plotting the Top Five Vehicles:
+barplot(top.five, names.arg = names(top.five), )
 
